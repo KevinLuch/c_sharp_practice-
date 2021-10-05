e7d6354fe54069f2d73ad537b433b6d0be70a16e
Kevin Luch <79926777+KevinLuch@users.noreply.github.com>
2021-10-05 12:16:46 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Yeezy";
+            char grade = 'A';
+            int age = 30;
+            double gpa = 3.2;
+            bool isMale = true;
+
+            Console.WriteLine(phrase + grade + age + gpa + isMale);
+        }
+    }
+}
